663dea69b0763c9ba4834bff281253d754680529
Naitik Shah <n@daaku.org>
2020-05-20 17:22:22 +0400

    initial commit

---
+++ main.go
@@ -0,0 +1,121 @@
+// Command ghbackup is a super simple github repository backup script.
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"os"
+	"path/filepath"
+	"sync"
+
+	"github.com/go-git/go-git/v5"
+	"github.com/google/go-github/v31/github"
+	"github.com/pkg/errors"
+	"golang.org/x/oauth2"
+)
+
+type refreshJob struct {
+	repo string
+	dest string
+}
+
+func cloneOrFetch(job refreshJob) error {
+	_, err := os.Stat(job.dest)
+	if err != nil {
+		if !os.IsNotExist(err) {
+			return errors.WithStack(err)
+		}
+
+		_, err = git.PlainClone(job.dest, true, &git.CloneOptions{
+			URL: job.repo,
+		})
+		return errors.WithStack(err)
+	}
+
+	repo, err := git.PlainOpen(job.dest)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	err = repo.Fetch(&git.FetchOptions{})
+	if err != nil && err != git.NoErrAlreadyUpToDate {
+		return errors.WithStack(err)
+	}
+	return nil
+}
+
+func refresh(wg *sync.WaitGroup, jobs chan refreshJob) {
+	defer wg.Done()
+	for job := range jobs {
+		if err := cloneOrFetch(job); err != nil {
+			fmt.Fprintf(os.Stderr, "for repo %q: %+v\n", job.repo, err)
+		}
+	}
+}
+
+func run() error {
+	fs := flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+	token := fs.String("token", "", "github token")
+	parallel := fs.Int("parallel", 5, "number of repositories to clone in parallel")
+	dest := fs.String("dest", "", "destination directory")
+	_ = fs.Parse(os.Args[1:])
+
+	if *token == "" {
+		return errors.New("-token must be provided")
+	}
+	if *dest == "" {
+		return errors.New("-dest must be provided")
+	}
+
+	jobs := make(chan refreshJob, *parallel*10)
+	var wg sync.WaitGroup
+	wg.Add(*parallel)
+	for i := 0; i < *parallel; i++ {
+		go refresh(&wg, jobs)
+	}
+
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{
+			AccessToken: *token,
+		},
+	)
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+
+	page := 0
+	for {
+		opts := github.RepositoryListOptions{
+			Affiliation: "owner",
+			ListOptions: github.ListOptions{
+				Page: page,
+			},
+		}
+		repos, resp, err := client.Repositories.List(ctx, "", &opts)
+		if err != nil {
+			return errors.WithStack(err)
+		}
+		for _, repo := range repos {
+			jobs <- refreshJob{
+				repo: *repo.SSHURL,
+				dest: filepath.Join(*dest, *repo.Name),
+			}
+		}
+		if resp.NextPage == 0 {
+			break
+		}
+		page = resp.NextPage
+	}
+
+	close(jobs)
+	wg.Wait()
+
+	return nil
+}
+
+func main() {
+	if err := run(); err != nil {
+		fmt.Fprintf(os.Stderr, "%+v\n", err)
+		os.Exit(1)
+	}
+}
---
+++ readme.md
@@ -0,0 +1,9 @@
+ghbackup
+========
+
+```
+ghbackup -token=$TOKEN -dest=$DIR
+```
+
+Will fetch all the users repositories into the dest directory (will fetch if
+they already exist). The repositories will be bare.
